90bf724e371eb07ae8f480544f1e8b6747620f9a
Krishan <kslg257@gmail.com>
2022-08-02 21:47:18 +0000

    Adding Categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -6,3 +6,24 @@ from taskmanager.models import Category, Task
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+# Two methods "GET" and "POST", since we will be submitting a form to the database.
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
+
+
+
+# Whenever you use the url_for() method on your links, it's important to note that these are
+# calling the actual Python functions (def home, etc), not the app.route, even though these are often the same name.
