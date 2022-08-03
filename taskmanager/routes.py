1d953bba1131ee6aaf049d4020ffc181e060d318
Krishan <kslg257@gmail.com>
2022-08-03 20:44:21 +0000

    READ from categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -10,7 +10,8 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
 
 # Two methods "GET" and "POST", since we will be submitting a form to the database.
